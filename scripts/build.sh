c44f6fa2d72daf3a1b282e82b27099b04bc5d65c
Didier Vidal <didier_devel@melix.net>
2020-04-25 23:15:50 +0200

    AJout d'un notebook de suivi des prédictions

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 cd "$(dirname "$0")"/..
 jupyter nbconvert --no-input --output-dir build --to html notebook/confinement.ipynb 
+jupyter nbconvert --no-input --output-dir build --execute --to html notebook/update.ipynb 
 
