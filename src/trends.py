64c7786da9806a409dd444a79091b1174d6034df
Didier Vidal <didier_devel@melix.net>
2021-09-07 22:05:00 +0200

    Retrait de Saint Martin des données hospitalieres car manque ailleurs

--- src/trends.py
+++ src/trends.py
@@ -71,6 +71,9 @@ urgence_df["reg_name"] = urgence_df["reg"].apply(lambda x: regs.loc[x].libelle i
 
 # Ajouter nom de département, code région, nom région dans les données des hospitalières
 hosp_df["dep"] = hosp_df["dep"].apply(lambda x: x if len(x) > 1 else '0'+x)
+#Retrait de Saint Martin
+hosp_df=hosp_df[hosp_df.dep != "978"]
+
 
 hosp_df["dep_name"] = hosp_df["dep"].apply(lambda x: depts.loc[str(x)].libelle if pd.notnull(x) else None)
 hosp_df["reg"] = hosp_df["dep"].apply(lambda x: depts.loc[x].reg if pd.notnull(x) else None)
